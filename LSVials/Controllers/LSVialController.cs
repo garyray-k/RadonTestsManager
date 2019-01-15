19d4c1116e79f6a150904ce7a4071605fd9d8ef3
Gary Krause <garyraykrause@outlook.com>
2019-01-15 06:31:03 -0500

    PUT and DELETE for LSVial

--- LSVials/Controllers/LSVialController.cs
+++ LSVials/Controllers/LSVialController.cs
@@ -55,12 +55,24 @@ namespace RadonTestsManager.LSVials.Controllers {
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value) {
+        public async Task<IActionResult> PutLSVialStatus(int id, [FromBody]string value) {
+            var lSVial = await _context.LSVials.FindAsync(id);
+            lSVial.Status = value;
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(
+                nameof(PutLSVialStatus),
+                new { id = id, status = ("This is my new status: " + value) });
+
         }
 
         // DELETE api/values/5
+        // TODO need to delete Job from DB first before removing vial (FK constraint)
         [HttpDelete("{id}")]
-        public void Delete(int id) {
+        public async Task<IActionResult> DeleteLSVial(int id) {
+            var lSVial = await _context.LSVials.FindAsync(id);
+            _context.LSVials.Remove(lSVial);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Get));
         }
     }
 }
